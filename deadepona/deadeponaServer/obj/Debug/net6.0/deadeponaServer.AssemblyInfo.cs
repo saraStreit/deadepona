4685f52b23dbcd8821ed804298e095d8af755519
Sara <Sara.streit@ag.ch>
2023-02-28 10:43:22 +0100

    add aspnet core server project

--- deadepona/deadeponaServer/obj/Debug/net6.0/deadeponaServer.AssemblyInfo.cs
+++ deadepona/deadeponaServer/obj/Debug/net6.0/deadeponaServer.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("deadepona")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("deadeponaServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("deadepona")]
-[assembly: System.Reflection.AssemblyTitleAttribute("deadepona")]
+[assembly: System.Reflection.AssemblyProductAttribute("deadeponaServer")]
+[assembly: System.Reflection.AssemblyTitleAttribute("deadeponaServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
